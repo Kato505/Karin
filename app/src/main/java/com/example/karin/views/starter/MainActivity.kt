3c22f15de73ceea608508282cb1b14f4a1461c69
PC <dibyhmind@gmail.com>
2025-11-18 23:46:39 +0600

    initial commit

--- app/src/main/java/com/example/karin/views/starter/MainActivity.kt
+++ app/src/main/java/com/example/karin/views/starter/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.karin
+package com.example.karin.views.starter
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.karin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
